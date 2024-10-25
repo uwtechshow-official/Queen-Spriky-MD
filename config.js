f7db060b940221d93f5c84058e86009834318ea2
Udavin Wijesundara <86962780+uwtechshow-official@users.noreply.github.com>
2024-10-25 22:56:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '7iJWlJQQ#LjnjSKFlJmudP7MLUm-HTTM6qHf3tP7ShEDkGhxc6yE', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || 'mongodb+srv://mdprabath518:1234@cluster0.swetgsg.mongodb.net/',// Enter Your MongoDB URL
+    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
     Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
 };
